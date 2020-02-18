6060688a96ea08a9aa71d02ed72efe0a2b6be997
Admin <admin@Admin.local>
2020-02-18 04:13:50 +0400

    Go back to db config and incorporate tabs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,73 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { add } from 'date-fns';
 
 import FirebaseContext from './context';
 import { firebaseConfig } from '../config/config';
 
 const secondaryApp = firebase.initializeApp(firebaseConfig, 'Secondary');
+
+// Create new order document
+export const createOrderDocument = async (orderData, additionalData) => {
+  const { customer, order, total, createdBy } = orderData;
+
+  const { id, activeTab, tabAmount, orders } = customer;
+
+  const userRef = firestore.doc(`users/${id}`);
+
+  const snapshot = await userRef.get();
+
+  if (snapshot.exists) {
+    // Create new order
+    const createdAt = new Date();
+
+    if (!activeTab) {
+      // Create new tab
+      const startDate = createdAt;
+      const dueDate = add(startDate, { months: 1 });
+
+      console.log(customer);
+
+      try {
+        await userRef.update({
+          orders: [
+            {
+              items: order,
+              total,
+              createdAt,
+              createdBy
+            }
+          ],
+          startDate,
+          dueDate,
+          activeTab: true,
+          tabAmount: total
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      // Update existing tab
+      try {
+        await userRef.update({
+          orders: [
+            ...orders,
+            {
+              items: order,
+              total,
+              createdAt,
+              createdBy
+            }
+          ],
+          tabAmount: tabAmount + total
+        });
+      } catch (error) {}
+    }
+  }
+
+  return userRef;
+};
 
 // Create user with firebase auth
 export const createNewUser = (email, password) => {
@@ -38,6 +100,12 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         role,
         telephone,
         email,
+        orders: [],
+        activeTab: false,
+        startDate: null,
+        dueDate: null,
+        tabAmount: 0,
+        seybrewTab: 0,
         createdBy,
         createdAt,
         ...additionalData
